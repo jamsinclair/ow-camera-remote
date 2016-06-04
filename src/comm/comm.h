de91d992cd5748ee2d470e9521d924f24a3f7666
jamsinclair <jamsinclairnz@gmail.com>
2016-06-04 17:31:42 +1200

    Bump the timeout to 4000 miliseconds

--- src/comm/comm.h
+++ src/comm/comm.h
@@ -2,7 +2,7 @@
 #define KEY_APP_STATUS_OKAY 200
 #define KEY_CAPTURE 1
 // Time to wait for a response from the companion app
-#define APP_MESSAGE_TIMEOUT 1500
+#define APP_MESSAGE_TIMEOUT 4000
 
 void send_int_app_message_with_callback();
 void init_comm();
