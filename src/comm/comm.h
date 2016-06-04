a48a7979194593a30fefbb0ca1e6ac6940b6e11a
jamsinclair <jamsinclairnz@gmail.com>
2016-06-04 22:53:40 +1200

    Add app msg picture taken constant

--- src/comm/comm.h
+++ src/comm/comm.h
@@ -1,6 +1,6 @@
 #define KEY_APP_STATUS_CHECK 0
-#define KEY_APP_STATUS_OKAY 200
 #define KEY_CAPTURE 1
+#define KEY_PICTURE_TAKEN 2
 // Time to wait for a response from the companion app
 #define APP_MESSAGE_TIMEOUT 4000
 
