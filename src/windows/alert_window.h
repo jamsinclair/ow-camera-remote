54e383b8883014de17648c154a48ce978a8a3b9b
jamsinclair <jamsinclairnz@gmail.com>
2016-06-04 13:57:30 +1200

    Add alert window

---
+++ src/windows/alert_window.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <pebble.h>
+
+#define DIALOG_MESSAGE_WINDOW_MESSAGE  "Unable to talk with app. Is 'Pebble Open Camera' app running?"
+#define DIALOG_MESSAGE_WINDOW_MARGIN   10
+
+void alert_window_push();
