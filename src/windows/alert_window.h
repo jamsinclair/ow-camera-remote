bb4257c45bb4d6de612a0fcf7837b44775e163cc
jamsinclair <jamsinclairnz@gmail.com>
2016-06-06 10:06:32 +1200

    Update alert window message with correct android app name

--- src/windows/alert_window.h
+++ src/windows/alert_window.h
@@ -2,7 +2,7 @@
 
 #include <pebble.h>
 
-#define DIALOG_MESSAGE_WINDOW_MESSAGE  "Unable to talk with app. Is 'Pebble Open Camera' app running?"
+#define DIALOG_MESSAGE_WINDOW_MESSAGE  "Unable to talk with app. Is 'OW Camera' Android app running?"
 #define DIALOG_MESSAGE_WINDOW_MARGIN   10
 
 void alert_window_push();
